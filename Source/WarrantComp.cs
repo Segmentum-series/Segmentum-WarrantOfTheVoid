709e0fb3d97366532e0cc3a8d279287f577b6ad6
TwoLargeGayMen <45555017+TwoLargeGayMen@users.noreply.github.com>
2026-07-12 15:04:03 -0500

    warrant fixes

--- Source/WarrantComp.cs
+++ Source/WarrantComp.cs
@@ -2,117 +2,127 @@ using Verse;
 using RimWorld;
 using System.Collections.Generic;
 using UnityEngine;
+
 namespace seg
 {
     public class SegWarrantGameComponent : GameComponent
-{
-    public static SegWarrantGameComponent Instance;
-
-    private List<Thing> warrantBuildings = new List<Thing>();
-
-    public SegWarrantGameComponent(Game game)
     {
-        Instance = this;
-    }
+        public static SegWarrantGameComponent Instance;
 
-    public override void GameComponentTick()
-    {
-        for (int i = warrantBuildings.Count - 1; i >= 0; i--)
+        private List<Thing> warrantBuildings = new List<Thing>();
+
+        public SegWarrantGameComponent(Game game)
         {
-            Thing thing = warrantBuildings[i];
+            Instance = this;
+        }
 
-            if (thing == null || thing.Destroyed)
+        public override void GameComponentTick()
+        {
+            for (int i = warrantBuildings.Count - 1; i >= 0; i--)
             {
-                warrantBuildings.RemoveAt(i);
-                continue;
-            }
+                Thing thing = warrantBuildings[i];
 
-            SegCompWarrantWall comp = thing.TryGetComp<SegCompWarrantWall>();
-            if (comp == null)
-            {
-                warrantBuildings.RemoveAt(i);
-                continue;
-            }
-
-            if (comp.owner != null && comp.owner.Dead)
-            {
-                if (comp.heir != null)
+                if (thing == null || thing.Destroyed)
                 {
-                    ApplyTitle(comp.heir);
+                    warrantBuildings.RemoveAt(i);
+                    continue;
                 }
 
-                RemoveTitle(comp.owner);
-                comp.owner = null;
+                SegCompWarrantWall comp = thing.TryGetComp<SegCompWarrantWall>();
+                if (comp == null)
+                {
+                    warrantBuildings.RemoveAt(i);
+                    continue;
+                }
+
+                if (comp.owner != null && comp.owner.Dead)
+                {
+                    if (comp.heir != null)
+                    {
+                        ApplyTitle(comp.heir);
+                    }
+
+                    RemoveTitle(comp.owner);
+                    comp.owner = null;
+                }
             }
+        }
+
+        public void Notify_OwnerAssigned(SegCompWarrantWall comp)
+        {
+            Thing parent = comp.parent;
+
+            if (!warrantBuildings.Contains(parent))
+                warrantBuildings.Add(parent);
+
+            ApplyTitle(comp.owner);
+        }
+
+        public void Notify_HeirAssigned(SegCompWarrantWall comp)
+        {
+            Thing parent = comp.parent;
+
+            if (!warrantBuildings.Contains(parent))
+                warrantBuildings.Add(parent);
+        }
+
+        public void Notify_WarrantDestroyed(SegCompWarrantWall comp)
+        {
+            Thing parent = comp.parent;
+
+            if (comp.owner != null)
+                RemoveTitle(comp.owner);
+
+            warrantBuildings.Remove(parent);
+        }
+
+        private void ApplyTitle(Pawn pawn)
+        {
+            Faction faction = Find.FactionManager.FirstFactionOfDef(
+                DefDatabase<FactionDef>.GetNamed("Seg_WOTV_ImperiumOfMan"));
+
+            RoyalTitleDef title = DefDatabase<RoyalTitleDef>.GetNamed("Seg_WOTV_RogueTraderTitle");
+
+            pawn.royalty.SetTitle(faction, title, grantRewards: false, rewardsOnlyForNewestTitle: false, sendLetter: false);
+
+            string letterTitle = "Seg_WOTV_RogueTraderTitle_LetterTitle".Translate(pawn.Named("PAWN"));
+            string letterBody = "Seg_WOTV_RogueTraderTitle_LetterBody".Translate(
+                pawn.Named("PAWN"),
+                faction.Named("FACTION")
+            );
+
+            Find.LetterStack.ReceiveLetter(letterTitle, letterBody, LetterDefOf.PositiveEvent, pawn);
+        }
+
+        private void RemoveTitle(Pawn pawn)
+        {
+            Faction faction = Find.FactionManager.FirstFactionOfDef(
+                DefDatabase<FactionDef>.GetNamed("Seg_WOTV_ImperiumOfMan"));
+
+            pawn.royalty.SetTitle(faction, null, grantRewards: false, rewardsOnlyForNewestTitle: false, sendLetter: false);
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_Collections.Look(ref warrantBuildings, "Seg_WOTV_warrantBuildings", LookMode.Reference);
         }
     }
 
-    public void Notify_OwnerAssigned(SegCompWarrantWall comp)
-    {
-        Thing parent = comp.parent;
-
-        if (!warrantBuildings.Contains(parent))
-            warrantBuildings.Add(parent);
-
-        ApplyTitle(comp.owner);
-    }
-
-    public void Notify_HeirAssigned(SegCompWarrantWall comp)
-    {
-        Thing parent = comp.parent;
-
-        if (!warrantBuildings.Contains(parent))
-            warrantBuildings.Add(parent);
-    }
-
-    public void Notify_WarrantDestroyed(SegCompWarrantWall comp)
-    {
-        Thing parent = comp.parent;
-
-        if (comp.owner != null)
-            RemoveTitle(comp.owner);
-
-        warrantBuildings.Remove(parent);
-    }
-
-    private void ApplyTitle(Pawn pawn)
-    {
-        Faction faction = Find.FactionManager.FirstFactionOfDef(
-            DefDatabase<FactionDef>.GetNamed("Ancients"));
-
-        RoyalTitleDef title = DefDatabase<RoyalTitleDef>.GetNamed("Seg_WOTV_RogueTraderTitle");
-
-        pawn.royalty.SetTitle(faction, title, grantRewards: false);
-    }
-
-    private void RemoveTitle(Pawn pawn)
-    {
-        Faction faction = Find.FactionManager.FirstFactionOfDef(
-            DefDatabase<FactionDef>.GetNamed("Ancients"));
-
-        pawn.royalty.SetTitle(faction, null, grantRewards: false);
-    }
-
-    public override void ExposeData()
-    {
-        Scribe_Collections.Look(ref warrantBuildings, "Seg_WOTV_warrantBuildings", LookMode.Reference);
-    }
-}
- public class CompProperties_SegCompWarrantWall : CompProperties
+    public class CompProperties_SegCompWarrantWall : CompProperties
     {
         public CompProperties_SegCompWarrantWall()
         {
             this.compClass = typeof(SegCompWarrantWall);
         }
     }
-      public class SegCompWarrantWall : ThingComp
+
+    public class SegCompWarrantWall : ThingComp
     {
         public Pawn owner;
         public Pawn heir;
 
         public override IEnumerable<Gizmo> CompGetGizmosExtra()
         {
-            // Assign Owner
             yield return new Command_Action
             {
                 defaultLabel = "Assign Warrant Holder",
@@ -130,26 +140,26 @@ namespace seg
                     }
                     Find.WindowStack.Add(new FloatMenu(opts));
                 }
-                };
+            };
 
-                    yield return new Command_Action
+            yield return new Command_Action
+            {
+                defaultLabel = "Assign Heir",
+                icon = ContentFinder<Texture2D>.Get("UI/Gizmos/Seg_WOTV_Gizmo_Heir"),
+                action = () =>
+                {
+                    List<FloatMenuOption> opts = new List<FloatMenuOption>();
+                    foreach (Pawn p in this.parent.Map.mapPawns.FreeColonists)
                     {
-                    defaultLabel = "Assign Heir",
-                    icon = ContentFinder<Texture2D>.Get("UI/Gizmos/Seg_WOTV_Gizmo_Heir"),
-                    action = () =>
-                    {
-                        List<FloatMenuOption> opts = new List<FloatMenuOption>();
-                        foreach (Pawn p in this.parent.Map.mapPawns.FreeColonists)
+                        opts.Add(new FloatMenuOption(p.LabelShort, () =>
                         {
-                            opts.Add(new FloatMenuOption(p.LabelShort, () =>
-                            {
-                                heir = p;
-                                SegWarrantGameComponent.Instance.Notify_HeirAssigned(this);
-                            }));
-                        }
-                        Find.WindowStack.Add(new FloatMenu(opts));
+                            heir = p;
+                            SegWarrantGameComponent.Instance.Notify_HeirAssigned(this);
+                        }));
                     }
-                };
+                    Find.WindowStack.Add(new FloatMenu(opts));
+                }
+            };
         }
 
         public override void PostDestroy(DestroyMode mode, Map previousMap)
